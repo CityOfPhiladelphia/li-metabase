69a4f9db5ef3605f0be232a436d52cbc3dd3c17d
Philip Ribbens <Philip.Ribbens@phila.gov>
2019-03-21 14:19:38 -0400

    More work on sla bl

--- etl/queries/SLA_BL.sql
+++ etl/queries/SLA_BL.sql
@@ -1,10 +1,11 @@
-SELECT DISTINCT j.jobid,
-  proc.processid,
+SELECT DISTINCT 
+  j.externalfilenum JobNumber,
+  ap.LICENSETYPESDISPLAYFORMAT LicenseType,
   REPLACE(jt.description, 'Business License ', '') JobType,
   ap.createddate JobCreatedDate,
-  proc.datecompleted ProcessDateCompleted,
-  j.externalfilenum JobNumber,
   stat.description JobStatus,
+  proc.processstatus CompletenessCheckStatus,
+  proc.datecompleted CompletenessCheckCompleted,
   (
   CASE
     WHEN jt.description LIKE 'Business License Application'
@@ -22,23 +23,24 @@ FROM api.processes PROC,
   api.statuses stat,
   api.jobtypes jt,
   query.j_bl_application ap
-WHERE j.jobid               = proc.jobid
-AND proc.processtypeid      = pt.processtypeid
-AND j.jobtypeid             = jt.jobtypeid
-AND j.jobid                 = ap.jobid
-AND j.statusid              = stat.statusid
-AND ap.createddate          > add_months(TRUNC(SYSDATE, 'MM'),-13)
-AND ap.createddate          < TO_DATE(TO_CHAR(sysdate, 'MM/DD/YYYY'), 'MM/DD/YYYY')
+WHERE j.jobid           = proc.jobid
+AND proc.processtypeid  = pt.processtypeid
+AND j.jobtypeid         = jt.jobtypeid
+AND j.jobid             = ap.jobid
+AND j.statusid          = stat.statusid
+AND ap.createddate      > add_months(TRUNC(SYSDATE, 'MM'),-13)
+AND ap.createddate      < TO_DATE(TO_CHAR(sysdate, 'MM/DD/YYYY'), 'MM/DD/YYYY')
 AND pt.processtypeid LIKE '1239327'
-AND j.statusid      IN ( '1036493', '978102', '1038535', '1039336') -- Approved, Submitted, Distribute, or In Adjudication
+AND j.statusid        IN ( '1036493', '978102', '1038535', '1039336') -- Approved, Submitted, Distribute, or In Adjudication
 UNION
-SELECT DISTINCT j.jobid,
-  proc.processid,
+SELECT DISTINCT 
+  j.externalfilenum JobNumber,
+  ar.LICENSETYPESDISPLAYFORMAT LicenseType,
   jt.description JobType,
   ar.createddate JobCreatedDate,
-  proc.datecompleted ProcessDateCompleted,
-  j.externalfilenum JobNumber,
   stat.description JobStatus,
+  proc.processstatus CompletenessCheckStatus,
+  proc.datecompleted CompletenessCheckCompleted,
   (
   CASE
     WHEN jt.description LIKE 'Business License Application'
@@ -56,12 +58,12 @@ FROM api.processes PROC,
   api.statuses stat,
   api.jobtypes jt,
   query.j_bl_amendrenew ar
-WHERE j.jobid               = proc.jobid
-AND proc.processtypeid      = pt.processtypeid
-AND j.jobtypeid             = jt.jobtypeid
-AND j.jobid                 = ar.jobid
-AND j.statusid              = stat.statusid
-AND ar.createddate          > add_months(TRUNC(SYSDATE, 'MM'),-13)
-AND ar.createddate          < TO_DATE(TO_CHAR(sysdate, 'MM/DD/YYYY'), 'MM/DD/YYYY')
+WHERE j.jobid          = proc.jobid
+AND proc.processtypeid = pt.processtypeid
+AND j.jobtypeid        = jt.jobtypeid
+AND j.jobid            = ar.jobid
+AND j.statusid         = stat.statusid
+AND ar.createddate     > add_months(TRUNC(SYSDATE, 'MM'),-13)
+AND ar.createddate     < TO_DATE(TO_CHAR(sysdate, 'MM/DD/YYYY'), 'MM/DD/YYYY')
 AND pt.processtypeid LIKE '1239327'
-AND j.statusid      IN ( '1036493', '978102', '1038535', '1039336') -- Approved, Submitted, Distribute, or In Adjudication
+AND j.statusid IN ( '1036493', '978102', '1038535', '1039336') -- Approved, Submitted, Distribute, or In Adjudication
--- etl/queries/SLA_BL_BUS_DAYS.sql
+++ etl/queries/SLA_BL_BUS_DAYS.sql
@@ -1,19 +1,20 @@
-SELECT sla.JOBID,
-  sla.PROCESSID,
+SELECT 
+  sla.jobnumber,
+  sla.licensetype,
   sla.jobtype,
   sla.JOBCREATEDDATE,
-  sla.PROCESSDATECOMPLETED,
-  sla.jobnumber,
   sla.JOBSTATUS,
+  sla.CompletenessCheckStatus,
+  sla.CompletenessCheckCompleted,  
   sla.joblink,
   bds1.BUSINESSDAYSSINCE BDSinceJobCreated,
   (
   CASE
-    WHEN sla.PROCESSDATECOMPLETED is not null
+    WHEN sla.CompletenessCheckCompleted is not null
     THEN 1
     ELSE 0
   END ) ProcessCompleted,
-  bds2.BUSINESSDAYSSINCE BDSinceProcessCompleted,
+  bds2.BUSINESSDAYSSINCE BDSinceCompletenessCheck,
   bds2.BUSINESSDAYSSINCE - bds1.BUSINESSDAYSSINCE BDOpen,
   (
   CASE
@@ -27,6 +28,6 @@ FROM SLA_BL sla,
 WHERE TO_DATE(TO_CHAR(sla.JOBCREATEDDATE, 'mm')
   || TO_CHAR(sla.JOBCREATEDDATE, 'dd')
   || TO_CHAR(sla.JOBCREATEDDATE, 'yyyy'), 'MMDDYYYY') = bds1.DATEOFYEAR (+)
-AND TO_DATE(TO_CHAR(sla.PROCESSDATECOMPLETED, 'mm')
-  || TO_CHAR(sla.PROCESSDATECOMPLETED, 'dd')
-  || TO_CHAR(sla.PROCESSDATECOMPLETED, 'yyyy'), 'MMDDYYYY') = bds2.DATEOFYEAR (+)
+AND TO_DATE(TO_CHAR(sla.CompletenessCheckCompleted, 'mm')
+  || TO_CHAR(sla.CompletenessCheckCompleted, 'dd')
+  || TO_CHAR(sla.CompletenessCheckCompleted, 'yyyy'), 'MMDDYYYY') = bds2.DATEOFYEAR (+)
