127355606a9713a83cb868613b515dd24e0f3e19
Philip Ribbens <Philip.Ribbens@phila.gov>
2019-03-28 13:05:17 -0400

    Add casegroup field to case_inspections table

--- etl/queries/cases_violations/case_inspections.sql
+++ etl/queries/cases_violations/case_inspections.sql
@@ -1,25 +1,30 @@
 SELECT
-	address,
-	zip,
-	census_tract_1990,
-	census_tract_2010,
-	council_district,
-	ops_district,
-	building_district,
-	ownername,
-	organization,
-	caseorpermitnumber,
-	aptype,
-	apdesc,
-	inspectorfirstname,
-	inspectorlastname,
-	inspectorfirstname || ' ' || inspectorlastname inspectorname,
-	inspectiontype,
-	inspectionscheduled,
-	inspectioncompleted,
-	inspectionstatus,
-	SDO_CS.TRANSFORM(SDO_GEOMETRY(2001,2272,SDO_POINT_TYPE(geocode_x, geocode_y,NULL),NULL,NULL), 4326).sdo_point.X lon,
-    SDO_CS.TRANSFORM(SDO_GEOMETRY(2001,2272,SDO_POINT_TYPE(geocode_x, geocode_y,NULL),NULL,NULL), 4326).sdo_point.Y lat
+	i.address,
+	i.zip,
+	i.census_tract_1990,
+	i.census_tract_2010,
+	i.council_district,
+	i.ops_district,
+	i.building_district,
+	i.ownername,
+	i.organization,
+	i.caseorpermitnumber casenumber,
+	i.aptype,
+	i.apdesc,
+	i.inspectorfirstname,
+	i.inspectorlastname,
+	i.inspectorfirstname || ' ' || inspectorlastname inspectorname,
+	i.inspectiontype,
+	i.inspectionscheduled,
+	i.inspectioncompleted,
+	i.inspectionstatus,
+  c.casegrp casegroup,
+	SDO_CS.TRANSFORM(SDO_GEOMETRY(2001,2272,SDO_POINT_TYPE(i.geocode_x, i.geocode_y,NULL),NULL,NULL), 4326).sdo_point.X lon,
+  SDO_CS.TRANSFORM(SDO_GEOMETRY(2001,2272,SDO_POINT_TYPE(i.geocode_x, i.geocode_y,NULL),NULL,NULL), 4326).sdo_point.Y lat
 FROM
-	inspections_all_internal_mvw
+	inspections_all_internal_mvw i
+LEFT OUTER JOIN imsv7.apcase@lidb_link c ON
+	i.apkey = c.apkey
 WHERE inspectionscheduled >= DATE '2017-1-1'
+AND inspectionscheduled < TO_DATE(TO_CHAR(SYSDATE,'MM/DD/YYYY'),'MM/DD/YYYY')
+AND aptype in ('CD ENFORCE','DANGEROUS','L_CLIP','L_DANGBLDG')
