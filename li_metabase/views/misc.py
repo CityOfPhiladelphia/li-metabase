f01c1000f3c34527c9dba7b946a215ebad86d952
Philip Ribbens <Philip.Ribbens@phila.gov>
2019-04-16 16:32:13 -0400

    Small fixes

--- li_metabase/views/misc.py
+++ li_metabase/views/misc.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 from li_metabase.utils import Dashboard, build_iframe_url_from_dashboard_url, DashboardNotFound
 from li_metabase.auth import auth
@@ -8,7 +8,7 @@ MISC_DASHBOARDS = [
     Dashboard('Individual Workloads', 'individual-workloads', 49),
     Dashboard('Expiring Licenses with Tax Issues', 'expiring-licenses-with-tax-issues', 72),
     Dashboard('Public Demos', 'public-demos', 76),
-    Dashboard('Uninspected Service Requests', 'unispected-service-requests', 77),
+    Dashboard('Uninspected Service Requests', 'uninspected-service-requests', 77),
 ]
 
 bp = Blueprint('misc', __name__)
@@ -25,3 +25,9 @@ def misc(dashboard_url):
 @bp.errorhandler(DashboardNotFound)
 def handle_error(error):
     return render_template('error.html')
+
+#Redirect old versions of URLs to new versions
+@bp.route('/misc/unispected-service-requests')
+@auth.login_required
+def uninspected_service_requests():
+    return redirect("/misc/uninspected-service-requests", code=301)
