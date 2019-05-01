d031a39039e4bc9f7c0b1ad4eb301970d83d7152
Philip Ribbens <Philip.Ribbens@phila.gov>
2019-05-01 14:58:46 -0400

    Add job status field to incomplete_processes_bl table

--- etl/queries/licenses/incomplete_processes_bl.sql
+++ etl/queries/licenses/incomplete_processes_bl.sql
@@ -1,6 +1,6 @@
 SELECT proc.processid,
   pt.description ProcessType,
-  j.ExternalFileNum JobNumber,  
+  j.ExternalFileNum JobNumber,
   REPLACE(jt.description, 'Business License ', '') JobType,
   (
   CASE
@@ -8,42 +8,48 @@ SELECT proc.processid,
     THEN ap.licensetypesdisplayformat
     ELSE '(none)'
   END ) LicenseType,
-  (CASE
+  (
+  CASE
     WHEN proc.ASSIGNEDSTAFF IS NULL
     THEN '(none)'
     WHEN regexp_count(proc.ASSIGNEDSTAFF, ',') = 0
     THEN proc.ASSIGNEDSTAFF
     ELSE 'multiple'
   END ) ASSIGNEDSTAFF,
-  (CASE
+  (
+  CASE
     WHEN regexp_count(proc.ASSIGNEDSTAFF, ',') IS NULL
     THEN 0
     ELSE regexp_count(proc.ASSIGNEDSTAFF, ',') + 1
   END ) NUMASSIGNEDSTAFF,
   proc.scheduledstartdate ScheduledStartDate,
   SYSDATE - proc.scheduledstartdate TimeSinceScheduledStartDate,
- (CASE
+  (
+  CASE
     WHEN jt.description LIKE 'Business License Application'
     THEN 'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=1239699&objectHandle='
       ||j.jobid
       ||'&processHandle='
       ||proc.processid
       ||'&paneId=1239699_151'
-  END ) JobLink
+  END ) JobLink,
+  stat.description JobStatus
 FROM api.processes PROC,
   api.processtypes pt,
   api.jobs j,
   api.jobtypes jt,
+  api.statuses stat,
   query.j_bl_application ap
-WHERE proc.jobid           = j.jobid
-AND j.jobid                = ap.jobid
-AND proc.processtypeid     = pt.processtypeid
-AND j.jobtypeid            = jt.jobtypeid
-AND proc.datecompleted     IS NULL
+WHERE proc.jobid             = j.jobid
+AND j.jobid                  = ap.jobid
+AND proc.processtypeid       = pt.processtypeid
+AND j.jobtypeid              = jt.jobtypeid
+AND j.statusid               = stat.statusid
+AND proc.datecompleted      IS NULL
 UNION
 SELECT proc.processid,
   pt.description ProcessType,
-  j.ExternalFileNum JobNumber,  
+  j.ExternalFileNum JobNumber,
   REPLACE(REPLACE(jt.description, 'Business License ', ''), 'Amendment/Renewal', 'Amend/Renew') JobType,
   (
   CASE
@@ -51,21 +57,23 @@ SELECT proc.processid,
     THEN ar.licensetypesdisplayformat
     ELSE '(none)'
   END ) LicenseType,
-  (CASE
+  (
+  CASE
     WHEN proc.ASSIGNEDSTAFF IS NULL
     THEN '(none)'
     WHEN regexp_count(proc.ASSIGNEDSTAFF, ',') = 0
     THEN proc.ASSIGNEDSTAFF
     ELSE 'multiple'
   END ) ASSIGNEDSTAFF,
-  (CASE
+  (
+  CASE
     WHEN regexp_count(proc.ASSIGNEDSTAFF, ',') IS NULL
     THEN 0
     ELSE regexp_count(proc.ASSIGNEDSTAFF, ',') + 1
   END ) NUMASSIGNEDSTAFF,
   proc.scheduledstartdate ScheduledStartDate,
   SYSDATE - proc.scheduledstartdate TimeSinceScheduledStartDate,
-    (
+  (
   CASE
     WHEN jt.description LIKE 'Amendment/Renewal'
     THEN 'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=1243107&objectHandle='
@@ -73,14 +81,17 @@ SELECT proc.processid,
       ||'&processHandle='
       ||proc.processid
       ||'&paneId=1243107_175'
-  END ) JobLink
+  END ) JobLink,
+  stat.description JobStatus
 FROM api.processes PROC,
   api.processtypes pt,
   api.jobs j,
   api.jobtypes jt,
+  api.statuses stat,
   query.j_bl_amendrenew ar
-WHERE proc.jobid           = j.jobid
-AND j.jobid                = ar.jobid
-AND proc.processtypeid     = pt.processtypeid
-AND j.jobtypeid            = jt.jobtypeid
-AND proc.datecompleted     IS NULL
+WHERE proc.jobid             = j.jobid
+AND j.jobid                  = ar.jobid
+AND proc.processtypeid       = pt.processtypeid
+AND j.jobtypeid              = jt.jobtypeid
+AND j.statusid               = stat.statusid
+AND proc.datecompleted      IS NULL
