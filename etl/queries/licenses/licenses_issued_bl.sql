d73bcf96afb7248181f0cb8a1e5ab9b8b4fdff0a
Philip Ribbens <Philip.Ribbens@phila.gov>
2019-07-18 10:01:08 -0400

    Update licenses_issued_bl query: use lmscorral instead of query, fix activity licenses portion of query

--- etl/queries/licenses/licenses_issued_bl.sql
+++ etl/queries/licenses/licenses_issued_bl.sql
@@ -1,48 +1,49 @@
 SELECT DISTINCT lt.name licensetype,
-  lic.externalfilenum licensenumber,
+  lic.licensenumber,
   ap.ExternalFileNum jobnumber,
   ap.applicationtype jobtype,
   ap.issuedate issuedate
-FROM query.j_bl_application ap,
-  query.r_bl_application_license apl,
-  query.o_bl_license lic,
+FROM lmscorral.bl_application ap,
+  LMSCORRAL.BL_APPLICATIONLICENSEXREF apl,
+  lmscorral.bl_license lic,
   lmscorral.bl_licensetype lt
-WHERE lic.licensetypeid     = lt.objectid
-AND lic.objectid            = apl.licenseobjectid
-AND apl.applicationobjectid = ap.jobid
+WHERE lic.licensetypeobjectid     = lt.objectid
+AND lic.objectid            = apl.licenseid
+AND apl.applicationid = ap.jobid
 AND ap.statusdescription LIKE 'Approved'
 AND ap.issuedate      >= '01-JAN-16'
 AND ap.IssueDate       < SYSDATE
 AND ap.applicationtype = 'Application'
 UNION
 SELECT DISTINCT lt.name licensetype,
-  lic.externalfilenum licensenumber,
+  lic.licensenumber,
   ar.ExternalFileNum jobnumber,
   ar.applicationtype jobtype,
   ar.issuedate issuedate
-FROM query.j_bl_amendrenew ar,
-  query.r_bl_amendrenew_license rla,
-  query.o_bl_license lic,
+FROM lmscorral.bl_amendmentrenewal ar,
+  LMSCORRAL.AMENDRENEWLICENSEXREF rla,
+  lmscorral.bl_license lic,
   lmscorral.bl_licensetype lt
-WHERE lic.licensetypeid = lt.objectid
-AND lic.objectid        = rla.licenseid
-AND rla.amendrenewid    = ar.jobid
+WHERE lic.licensetypeobjectid = lt.objectid
+AND lic.objectid        = rla.licenseobjectid
+AND rla.amendrenewjobid    = ar.jobid
 AND ar.statusdescription LIKE 'Approved'
 AND ar.issuedate >= '01-JAN-16'
 AND ar.issuedate  < SYSDATE
 AND ar.applicationtype LIKE 'Renewal'
 UNION
 SELECT DISTINCT lt.name licensetype,
-  lic.externalfilenum licensenumber,
+  lic.licensenumber,
   ap.ExternalFileNum jobnumber,
   ap.applicationtype jobtype,
-  ap.IssueDate issuedate
-FROM query.o_bl_license lic,
-  lmscorral.bl_licensetype lt,
-  query.j_bl_application ap
-WHERE lic.licensetypeid   = lt.objectid
-AND lic.InitialIssueDate >= '01-JAN-16'
-AND lic.InitialIssueDate  < SYSDATE
+  ap.issuedate
+FROM lmscorral.bl_application ap,
+  lmscorral.bl_license lic,
+  lmscorral.bl_licensetype lt
+WHERE lic.licensetypeobjectid   = lt.objectid
+AND ap.issuedate >= '01-JAN-16'
+AND ap.issuedate  < SYSDATE
 AND lt.Name LIKE 'Activity'
 AND lic.ObjectId = ap.ActivityLicenseId
 AND ap.statusdescription LIKE 'Approved'
+AND ap.ISACTIVITYLICENSEAPP = 'Y'
