942ee45e3267622de640de9565323d15e76a4ded
Philip Ribbens <Philip.Ribbens@phila.gov>
2019-10-07 11:27:42 -0400

    Revert "Remove ETL stuff for Case Supplementary Logs dashboards since we're putting that one in a separate app"
    This reverts commit f2f3025f6ebb0b67d34bc7db0384001cde6a245d.

--- etl/sql_queries.py
+++ etl/sql_queries.py
@@ -258,6 +258,12 @@ DumpsterMedallionIDLookup = SqlQuery(
     target_table = 'DUMPSTERLICENSESMEDALLIONID'
 )
 
+CasesInspectorsLogs = SqlQuery(
+    extract_query_file = 'cases_violations/cases_inspectors_logs.sql',
+    source_db = 'GISLNI',
+    target_table = 'CASES_INSPECTORS_LOGS'
+)
+
 TLPermitsFPPInsp = SqlQuery(
     extract_query_file = 'misc/tl_permits_fpp_insp.sql',
     source_db = 'GISLNI',
@@ -307,5 +313,6 @@ queries = [
     InspectionsCompletedAll,
     InspectionsCompletedAllFuture,
 	DumpsterMedallionIDLookup,
-    TLPermitsFPPInsp
+    TLPermitsFPPInsp,
+    CasesInspectorsLogs
 ]
