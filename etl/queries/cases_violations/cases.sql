ea46de511524aea674719dc55e098830de8d7250
Philip Ribbens <Philip.Ribbens@phila.gov>
2019-12-17 15:36:15 -0500

    Add field to Cases table for whether or not the case only includes license violations:

--- etl/queries/cases_violations/cases.sql
+++ etl/queries/cases_violations/cases.sql
@@ -53,7 +53,13 @@ SELECT TRIM(c.apno) casenumber,
     WHEN fci.inspectorname IS NULL
     THEN '(none)'
     ELSE fci.inspectorname
-  END ) FirstCompletedInspInspector
+  END ) FirstCompletedInspInspector,
+  (
+  CASE
+    WHEN cases_with_only_license_vios.casenumber IS NOT NULL
+    THEN 'Yes'
+    ELSE 'No'
+  END) OnlyIncludesLicenseVios
 FROM imsv7.apcase@lidb_link c,
   (SELECT *
   FROM
@@ -145,12 +151,27 @@ FROM imsv7.apcase@lidb_link c,
   WHERE RankSchedDttm = 1
   ) ovdi,
   imsv7.address@lidb_link a,
-  lni_addr
-WHERE c.apkey  = fci.apkey (+)
-AND c.apkey    = lci.apkey (+)
-AND c.apkey    = nsi.apkey (+)
-AND c.apkey    = ovdi.apkey (+)
-AND c.addrkey  = a.addrkey
-AND c.addrkey  = lni_addr.addrkey (+)
-AND c.adddttm >= add_months(TRUNC(SYSDATE, 'MM'),-60)
-AND c.adddttm  < TO_DATE(TO_CHAR(SYSDATE,'MM/DD/YYYY'),'MM/DD/YYYY')
+  lni_addr,
+  ( SELECT DISTINCT casenumber
+  FROM VIOLATIONS_MVW
+  WHERE (VIOLATIONTYPE IN ('9-3902 (1)','9-3902 (2)','9-3902 (3)','9-3902 (4)','9-3904','9-3905')
+  OR VIOLATIONTYPE NOT LIKE 'PM-102%')
+  AND CASEADDEDDATE >= add_months(TRUNC(SYSDATE, 'MM'),-60)
+  AND CASEADDEDDATE  < TO_DATE(TO_CHAR(SYSDATE,'MM/DD/YYYY'),'MM/DD/YYYY')
+  MINUS
+  SELECT DISTINCT casenumber
+  FROM VIOLATIONS_MVW
+  WHERE VIOLATIONTYPE NOT IN ('9-3902 (1)','9-3902 (2)','9-3902 (3)','9-3902 (4)','9-3904','9-3905')
+  AND VIOLATIONTYPE NOT LIKE 'PM-102%'
+  AND CASEADDEDDATE >= add_months(TRUNC(SYSDATE, 'MM'),-60)
+  AND CASEADDEDDATE  < TO_DATE(TO_CHAR(SYSDATE,'MM/DD/YYYY'),'MM/DD/YYYY')
+  ) cases_with_only_license_vios
+WHERE c.apkey    = fci.apkey (+)
+AND c.apkey      = lci.apkey (+)
+AND c.apkey      = nsi.apkey (+)
+AND c.apkey      = ovdi.apkey (+)
+AND c.addrkey    = a.addrkey
+AND c.addrkey    = lni_addr.addrkey (+)
+AND TRIM(c.apno) = cases_with_only_license_vios.casenumber (+)
+AND c.adddttm   >= add_months(TRUNC(SYSDATE, 'MM'),-60)
+AND c.adddttm    < TO_DATE(TO_CHAR(SYSDATE,'MM/DD/YYYY'),'MM/DD/YYYY')
