d63ebd5f538319571fc41582c4a57970c4329e52
Philip Ribbens <Philip.Ribbens@phila.gov>
2019-12-31 12:58:57 -0500

    Change cases query to reference CASES_MVW instead of Hansen

--- etl/queries/cases_violations/cases.sql
+++ etl/queries/cases_violations/cases.sql
@@ -1,157 +1,30 @@
-SELECT TRIM(c.apno) casenumber,
-  c.adddttm caseaddeddate,
-  (
-  CASE
-    WHEN c.rescode IS NULL
-    THEN '(none)'
-    ELSE c.rescode
-  END ) caseresolutioncode,
-  c.RESDTTM caseresolutiondate,
-  c.casegrp casegroup,
-  lci.compdttm LastCompletedInsp,
-  DECODE(lci.stat, 0, 'None', 1, 'Passed', 2, 'Failed', 3, 'Cancelled', 4, 'HOLD', 5, 'Closed') LastCompletedInspStatus,
-  (
-  CASE
-    WHEN lci.inspectorname IS NULL
-    THEN '(none)'
-    ELSE lci.inspectorname
-  END ) LastCompletedInspInspector,
-  nsi.SCHEDDTTM NextScheduledInsp,
-  (
-  CASE
-    WHEN nsi.inspectorname IS NULL
-    THEN '(none)'
-    ELSE nsi.inspectorname
-  END ) NextScheduledInspInspector,
-  ovdi.SCHEDDTTM OverdueInspScheduledDate,
-  (
-  CASE
-    WHEN ovdi.inspectorname IS NULL
-    THEN '(none)'
-    ELSE ovdi.inspectorname
-  END ) OverdueInspInspector,
-  (
-  CASE
-    WHEN a.zip IS NULL
-    OR a.zip    = ' '
-    THEN '(none)'
-    ELSE a.zip
-  END ) zip,
-  a.council CouncilDistrict,
-  lni_addr.OPS_DISTRICT OpsDistrict,
-  a.C_TRACT CensusTract,
-  (
-  CASE
-    WHEN c.RESDTTM IS NULL
-    THEN 'No'
-    ELSE 'Yes'
-  END ) caseresolutiondatehasvalue,
-  fci.compdttm FirstCompletedInsp,
-  DECODE(fci.stat, 0, 'None', 1, 'Passed', 2, 'Failed', 3, 'Cancelled', 4, 'HOLD', 5, 'Closed') FirstCompletedInspStatus,
-  (
-  CASE
-    WHEN fci.inspectorname IS NULL
-    THEN '(none)'
-    ELSE fci.inspectorname
-  END ) FirstCompletedInspInspector,
+SELECT c.casenumber,
+  c.caseaddeddate,
+  c.caseresolutioncode,
+  c.caseresolutiondate,
+  c.casegroup,
+  c.LastCompletedInsp,
+  c.LastCompletedInspStatus,
+  c.LastCompletedInspInspector,
+  c.NextScheduledInsp,
+  c.NextScheduledInspInspector,
+  c.OverdueInspScheduledDate,
+  c.OverdueInspInspector,
+  c.zip,
+  c.CouncilDistrict,
+  c.OpsDistrict,
+  c.CensusTract,
+  c.caseresolutiondatehasvalue,
+  c.FirstCompletedInsp,
+  c.FirstCompletedInspStatus,
+  c.FirstCompletedInspInspector,
   (
   CASE
     WHEN cases_with_only_license_vios.casenumber IS NOT NULL
     THEN 'Yes'
     ELSE 'No'
   END) OnlyIncludesLicenseVios
-FROM imsv7.apcase@lidb_link c,
-  (SELECT *
-  FROM
-    (SELECT RANK() OVER (PARTITION BY i.apkey ORDER BY i.compdttm ASC, i.apinspkey ASC NULLS LAST) AS RankCompDttm,
-      i.apkey,
-      i.SCHEDDTTM,
-      i.compdttm,
-      i.stat,
-      (
-      CASE
-        WHEN e.empfirst IS NULL
-        AND e.emplast   IS NULL
-        THEN '(none)'
-        ELSE e.empfirst
-          || ' '
-          || e.emplast
-      END ) inspectorname
-    FROM imsv7.apinsp@lidb_link i,
-      imsv7.employee@lidb_link e
-    WHERE i.assignto = e.empid (+)
-    AND i.compdttm  IS NOT NULL
-    )
-  WHERE RankCompDttm = 1
-  ) fci,
-  (SELECT *
-  FROM
-    (SELECT RANK() OVER (PARTITION BY i.apkey ORDER BY i.compdttm DESC, i.apinspkey DESC NULLS LAST) AS RankCompDttm,
-      i.apkey,
-      i.SCHEDDTTM,
-      i.compdttm,
-      i.stat,
-      (
-      CASE
-        WHEN e.empfirst IS NULL
-        AND e.emplast   IS NULL
-        THEN '(none)'
-        ELSE e.empfirst
-          || ' '
-          || e.emplast
-      END ) inspectorname
-    FROM imsv7.apinsp@lidb_link i,
-      imsv7.employee@lidb_link e
-    WHERE i.assignto = e.empid (+)
-    AND i.compdttm  IS NOT NULL
-    )
-  WHERE RankCompDttm = 1
-  ) lci,
-  (SELECT *
-  FROM
-    (SELECT RANK() OVER (PARTITION BY i.apkey ORDER BY i.SCHEDDTTM ASC, i.apinspkey ASC NULLS LAST) AS RankSchedDttm,
-      i.apkey,
-      i.SCHEDDTTM,
-      (
-      CASE
-        WHEN e.empfirst IS NULL
-        AND e.emplast   IS NULL
-        THEN '(none)'
-        ELSE e.empfirst
-          || ' '
-          || e.emplast
-      END ) inspectorname
-    FROM imsv7.apinsp@lidb_link i,
-      imsv7.employee@lidb_link e
-    WHERE i.assignto = e.empid (+)
-    AND i.SCHEDDTTM  > SYSDATE
-    )
-  WHERE RankSchedDttm = 1
-  ) nsi,
-  (SELECT *
-  FROM
-    (SELECT RANK() OVER (PARTITION BY i.apkey ORDER BY i.SCHEDDTTM ASC, i.apinspkey ASC NULLS LAST) AS RankSchedDttm,
-      i.apkey,
-      i.SCHEDDTTM,
-      (
-      CASE
-        WHEN e.empfirst IS NULL
-        AND e.emplast   IS NULL
-        THEN '(none)'
-        ELSE e.empfirst
-          || ' '
-          || e.emplast
-      END ) inspectorname
-    FROM imsv7.apinsp@lidb_link i,
-      imsv7.employee@lidb_link e
-    WHERE i.assignto = e.empid (+)
-    AND i.SCHEDDTTM  < SYSDATE
-    AND i.compdttm  IS NULL
-    )
-  WHERE RankSchedDttm = 1
-  ) ovdi,
-  imsv7.address@lidb_link a,
-  lni_addr,
+FROM CASES_MVW c,
   ( SELECT DISTINCT casenumber
   FROM VIOLATIONS_MVW
   WHERE (VIOLATIONTYPE IN ('9-3902 (1)','9-3902 (2)','9-3902 (3)','9-3902 (4)','9-3904','9-3905')
@@ -166,12 +39,6 @@ FROM imsv7.apcase@lidb_link c,
   AND CASEADDEDDATE >= add_months(TRUNC(SYSDATE, 'MM'),-60)
   AND CASEADDEDDATE  < TO_DATE(TO_CHAR(SYSDATE,'MM/DD/YYYY'),'MM/DD/YYYY')
   ) cases_with_only_license_vios
-WHERE c.apkey    = fci.apkey (+)
-AND c.apkey      = lci.apkey (+)
-AND c.apkey      = nsi.apkey (+)
-AND c.apkey      = ovdi.apkey (+)
-AND c.addrkey    = a.addrkey
-AND c.addrkey    = lni_addr.addrkey (+)
-AND TRIM(c.apno) = cases_with_only_license_vios.casenumber (+)
-AND c.adddttm   >= add_months(TRUNC(SYSDATE, 'MM'),-60)
-AND c.adddttm    < TO_DATE(TO_CHAR(SYSDATE,'MM/DD/YYYY'),'MM/DD/YYYY')
+WHERE c.casenumber   = cases_with_only_license_vios.casenumber (+)
+AND c.caseaddeddate >= add_months(TRUNC(SYSDATE, 'MM'),-60)
+AND c.caseaddeddate  < TO_DATE(TO_CHAR(SYSDATE,'MM/DD/YYYY'),'MM/DD/YYYY')
