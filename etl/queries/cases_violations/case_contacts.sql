8d69e02b539d767c028fa8cb3078adeddc234aac
Philip Ribbens <Philip.Ribbens@phila.gov>
2020-01-28 11:54:41 -0500

    Format

--- etl/queries/cases_violations/case_contacts.sql
+++ etl/queries/cases_violations/case_contacts.sql
@@ -1,48 +1,53 @@
 SELECT DISTINCT c.casenumber,
-  c.casegroup,
-  c.caseaddeddate,
-  c.caseresolutioncode,
-  c.caseresolutiondate,
-  c.contactaddress,
-  c.contactprimary,
-  c.contactcapacity,
-  c.contactname,
-  c.contactorganization,
-  c.casestatus,
-  tl.LICENSENUMBER ,
-  (
-  CASE
-    WHEN tl.LICENSETYPE IS NOT NULL
-    THEN tl.LICENSETYPE
-    ELSE '(N/A)'
-  END) licensetype,
-  (
-  CASE
-    WHEN tl.LICENSETYPE IS NOT NULL
-    THEN 'Yes'
-    ELSE 'No'
-  END) licenseholder,
-  c.caseaddress,
-  (
-  CASE
-    WHEN tl.CONTACTNAME IS NOT NULL
-    THEN UPPER(tl.CONTACTNAME)
-    ELSE '(N/A)'
-  END) LICENSECONTACTNAME,
-  (
-  CASE
-    WHEN tl.COMPANYNAME IS NOT NULL
-    THEN UPPER(tl.COMPANYNAME)
-    ELSE '(N/A)'
-  END) LICENSECOMPANYNAME,
-  tl.ISSUEDATE LicenseIssueDate,
-  tl.EXPIRATIONDATE LicenseExpirationDate,
-  (
-  CASE
-    WHEN tl.LICENSESTATUS IS NOT NULL
-    THEN tl.LICENSESTATUS
-    ELSE '(N/A)'
-  END) LICENSESTATUS
-FROM CASE_CONTACTS_MVW c,
-  TRADE_LICENSES_ALL_MVW tl
-WHERE c.LICENSENUM = tl.LICENSENUMBER (+)
+                c.casegroup,
+                c.caseaddeddate,
+                c.caseresolutioncode,
+                c.caseresolutiondate,
+                c.contactaddress,
+                c.contactprimary,
+                c.contactcapacity,
+                c.contactname,
+                c.contactorganization,
+                c.casestatus,
+                tl.licensenumber,
+                (
+                    CASE
+                        WHEN tl.licensetype IS NOT NULL
+                        THEN tl.licensetype
+                        ELSE '(N/A)'
+                    END
+                ) licensetype,
+                (
+                    CASE
+                        WHEN tl.licensetype IS NOT NULL
+                        THEN 'Yes'
+                        ELSE 'No'
+                    END
+                ) licenseholder,
+                c.caseaddress,
+                (
+                    CASE
+                        WHEN tl.contactname IS NOT NULL
+                        THEN upper (tl.contactname)
+                        ELSE '(N/A)'
+                    END
+                ) licensecontactname,
+                (
+                    CASE
+                        WHEN tl.companyname IS NOT NULL
+                        THEN upper (tl.companyname)
+                        ELSE '(N/A)'
+                    END
+                ) licensecompanyname,
+                tl.issuedate licenseissuedate,
+                tl.expirationdate licenseexpirationdate,
+                (
+                    CASE
+                        WHEN tl.licensestatus IS NOT NULL
+                        THEN tl.licensestatus
+                        ELSE '(N/A)'
+                    END
+                ) licensestatus
+FROM case_contacts_mvw c,
+     trade_licenses_all_mvw tl
+WHERE c.licensenum = tl.licensenumber (+)
