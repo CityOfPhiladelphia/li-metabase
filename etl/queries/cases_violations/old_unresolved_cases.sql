6f541f250c8a6551e6117fb0ba24ef0c8acf5c0b
Philip Ribbens <Philip.Ribbens@phila.gov>
2020-01-28 11:36:38 -0500

    Format and clean up old_unresolved_cases

--- etl/queries/cases_violations/old_unresolved_cases.sql
+++ etl/queries/cases_violations/old_unresolved_cases.sql
@@ -1,23 +1,24 @@
 SELECT c.casenumber,
-  c.caseaddeddate,
-  c.caseresolutioncode,
-  c.completeddate, --same as old caseresolutiondate?
-  c.caseresponsibility, --replacement for casegroup
-  c.LastCompletedInv,
-  c.LastCompletedInvStatus,
-  c.LastCompletedInvInvestigator,
-  c.NextScheduledInv,
-  c.NextScheduledInvInvestigator,
-  c.OverdueInvScheduledDate,
-  c.OverdueInvInvestigator,
-  c.zip,
-  c.council_district,
-  c.li_district,
-  c.census_tract_2010,
-  c.casecompleteddatehasvalue,
-  c.FirstCompletedInv,
-  c.FirstCompletedInvStatus,
-  c.FirstCompletedInvInvestigator
-FROM CASES_MVW c
-WHERE c.caseaddeddate < add_months(TRUNC(SYSDATE, 'MM'),-60)
-AND (c.caseresolutioncode is null or c.completeddate is null)
+       c.caseaddeddate,
+       c.casestatus,
+       c.completeddate,
+       c.caseresponsibility,
+       c.lastcompletedinv,
+       c.lastcompletedinvstatus,
+       c.lastcompletedinvinvestigator,
+       c.nextscheduledinv,
+       c.nextscheduledinvinvestigator,
+       c.overdueinvscheduleddate,
+       c.overdueinvinvestigator,
+       c.zip,
+       c.council_district,
+       c.li_district,
+       c.census_tract_2010,
+       c.casecompleteddatehasvalue,
+       c.firstcompletedinv,
+       c.firstcompletedinvstatus,
+       c.firstcompletedinvinvestigator
+FROM cases_mvw c
+WHERE c.caseaddeddate < add_months (trunc (sysdate, 'MM'), - 60)
+      AND (c.casestatus <> 'Closed'
+           OR c.completeddate IS NULL)
