0108fd4014a38dcbe4a4b5e6099405509284ef97
Philip Ribbens <Philip.Ribbens@phila.gov>
2020-04-27 11:13:37 -0400

    Refactor active jobs bl and active processes bl

--- etl/UpdateTables/queries/licenses/active_jobs_bl.sql
+++ etl/UpdateTables/queries/licenses/active_jobs_bl.sql
@@ -1,83 +1,33 @@
-SELECT DISTINCT allj.jobnumber,
-                allj.jobtype,
-                allj.licensetype,
-                allj.jobstatus,
+SELECT DISTINCT j.jobnumber,
+                j.jobtype,
+                j.licensetype,
+                j.jobstatus,
                 proc.processid,
-                proc.objectdefdescription processtype,
+                proc.processtype,
                 proc.datecompleted jobaccepteddate,
                 proc.processstatus,
                 proc.staffassigned assignedstaff,
+                proc.timesincescheduledstartdate,
                 (
                     CASE
-                        WHEN round (sysdate - proc.scheduledstartdate) <= 1
-                        THEN '0-1 Day'
-                        WHEN round (sysdate - proc.scheduledstartdate) BETWEEN 2 AND 5
-                        THEN '2-5 Days'
-                        WHEN round (sysdate - proc.scheduledstartdate) BETWEEN 6 AND 10
-                        THEN '6-10 Days'
-                        WHEN round (sysdate - proc.scheduledstartdate) BETWEEN 11 AND 365
-                        THEN '11 Days-1 Year'
-                        ELSE 'Over 1 Year'
-                    END
-                ) timesincescheduledstartdate,
-                (
-                    CASE
-                        WHEN allj.jobtype LIKE 'Application'
+                        WHEN j.jobtype LIKE 'Application'
                         THEN 'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=1239699&objectHandle='
-                        || allj.jobid || '&processHandle=&paneId=1239699_151'
-                        WHEN allj.jobtype LIKE 'Renewal'
+                        || j.jobid || '&processHandle=&paneId=1239699_151'
+                        WHEN j.jobtype LIKE 'Renewal'
                         THEN 'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=1243107&objectHandle='
-                        || allj.jobid || '&processHandle=&paneId=1243107_175'
+                        || j.jobid || '&processHandle=&paneId=1243107_175'
                     END
                 ) joblink
-FROM (SELECT DISTINCT ap.jobid,
-                      ap.externalfilenum jobnumber,
-                      ap.applicationtype jobtype,
-                      lt.name licensetype,
-                      ap.statusdescription jobstatus
-      FROM lmscorral.bl_application ap,
-           lmscorral.bl_applicationlicensexref aplx,
-           lmscorral.bl_license lic,
-           lmscorral.bl_licensetype lt,
-           lmscorral.bl_alljobs allj
-      WHERE ap.jobid = aplx.applicationid
-            AND aplx.licenseid           = lic.objectid
-            AND lic.licensetypeobjectid  = lt.objectid
-            AND ap.jobid                 = allj.jobid
-            AND allj.completeddate IS NULL
-            AND ap.statusdescription NOT IN (
-          'More Information Required',
-          'Payment Pending',
-          'Application Incomplete',
-          'Draft'
-      )
-  --AND ap.STATUSDESCRIPTION IN ('Distribute', 'In Adjudication','Submitted')
-      UNION
-      SELECT DISTINCT ar.jobid,
-                      ar.externalfilenum jobnumber,
-                      ar.applicationtype jobtype,
-                      lt.name licensetype,
-                      ar.statusdescription jobstatus
-      FROM lmscorral.bl_amendmentrenewal ar,
-           lmscorral.amendrenewlicensexref arlx,
-           lmscorral.bl_license lic,
-           lmscorral.bl_licensetype lt,
-           lmscorral.bl_alljobs allj
-      WHERE ar.jobid = arlx.amendrenewjobid
-            AND arlx.licenseobjectid     = lic.objectid
-            AND lic.licensetypeobjectid  = lt.objectid
-            AND ar.jobid                 = allj.jobid
-            AND allj.completeddate IS NULL
-            AND ar.statusdescription NOT IN (
-          'More Information Required',
-          'Payment Pending',
-          'Application Incomplete',
-          'Draft'
-      )
-    --AND ap.STATUSDESCRIPTION IN ('Distribute', 'In Adjudication','Submitted')
-) allj,
-     lmscorral.processes proc
-WHERE allj.jobid = proc.jobid
-      AND proc.objectdefdescription = 'Completeness Check'
+FROM g_mvw_bl_jobs j,
+     g_mvw_processes proc
+WHERE j.jobid = proc.jobid
+      AND j.jobcompleteddate IS NULL
+      AND j.jobstatus NOT IN (
+    'More Information Required',
+    'Payment Pending',
+    'Application Incomplete',
+    'Draft'
+)
+      AND proc.processtype = 'Completeness Check'
       AND proc.datecompleted IS NOT NULL
-ORDER BY allj.jobnumber
+ORDER BY j.jobnumber
--- etl/UpdateTables/queries/licenses/active_processes_bl.sql
+++ etl/UpdateTables/queries/licenses/active_processes_bl.sql
@@ -1,84 +1,38 @@
-SELECT DISTINCT *
-FROM (SELECT allj.jobnumber,
-             allj.jobtype,
-             (
-                 CASE
-                     WHEN allj.licensetype IS NOT NULL
-                     THEN allj.licensetype
-                     ELSE '(none)'
-                 END
-             ) licensetype,
-             allj.jobstatus,
-             proc.processid processid,
-             proc.objectdefdescription processtype,
-             proc.createddate createddate,
-             proc.scheduledstartdate scheduledstartdate,
-             proc.processstatus processstatus,
-             (
-                 CASE
-                     WHEN round (sysdate - proc.scheduledstartdate) <= 1
-                     THEN '0-1 Day'
-                     WHEN round (sysdate - proc.scheduledstartdate) BETWEEN 2 AND 5
-                     THEN '2-5 Days'
-                     WHEN round (sysdate - proc.scheduledstartdate) BETWEEN 6 AND 10
-                     THEN '6-10 Days'
-                     WHEN round (sysdate - proc.scheduledstartdate) BETWEEN 11 AND 365
-                     THEN '11 Days-1 Year'
-                     ELSE 'Over 1 Year'
-                 END
-             ) timesincescheduledstartdate,
-             (
-                 CASE
-                     WHEN allj.jobtype LIKE 'Application'
-                     THEN 'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=1239699&objectHandle='
-                     || allj.jobid || '&processHandle=' || proc.processid || '&paneId=1239699_151'
-                     WHEN allj.jobtype LIKE 'Renewal'
-                     THEN 'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=1243107&objectHandle='
-                     || allj.jobid || '&processHandle=' || proc.processid || '&paneId=1243107_175'
-                 END
-             ) processlink
-      FROM (SELECT DISTINCT ap.jobid,
-                            ap.externalfilenum jobnumber,
-                            ap.applicationtype jobtype,
-                            lt.name licensetype,
-                            ap.statusdescription jobstatus
-            FROM lmscorral.bl_application ap,
-                 lmscorral.bl_joblicensetypexref jltx,
-                 lmscorral.bl_licensetype lt,
-                 lmscorral.bl_alljobs allj
-            WHERE ap.jobid = jltx.jobid (+)
-                  AND jltx.licenseobjectid  = lt.objectid (+)
-                  AND ap.jobid              = allj.jobid
-                  AND ap.statusdescription NOT IN (
-                'Approved',
-                'Denied',
-                'Draft',
-                'Withdrawn',
-                'More Information Required'
-            )
-            UNION
-            SELECT DISTINCT ar.jobid,
-                            ar.externalfilenum jobnumber,
-                            ar.applicationtype jobtype,
-                            lt.name licensetype,
-                            ar.statusdescription jobstatus
-            FROM lmscorral.bl_amendmentrenewal ar,
-                 lmscorral.bl_joblicensetypexref jltx,
-                 lmscorral.bl_licensetype lt,
-                 lmscorral.bl_alljobs allj
-            WHERE ar.jobid = jltx.jobid (+)
-                  AND jltx.licenseobjectid  = lt.objectid (+)
-                  AND ar.jobid              = allj.jobid
-                  AND ar.statusdescription NOT IN (
-                'Approved',
-                'Denied',
-                'Draft',
-                'Withdrawn',
-                'More Information Required'
-            )
-      ) allj,
-           lmscorral.processes proc
-      WHERE allj.jobid = proc.jobid
-            AND proc.datecompleted IS NULL
-            AND proc.objectdefdescription <> 'Pay Fees'
-     )
+SELECT j.jobnumber,
+       j.jobtype,
+       (
+           CASE
+               WHEN j.licensetype IS NOT NULL
+               THEN j.licensetype
+               ELSE '(none)'
+           END
+       ) licensetype,
+       j.jobstatus,
+       proc.processid,
+       proc.processtype,
+       proc.createddate,
+       proc.scheduledstartdate,
+       proc.processstatus,
+       proc.timesincescheduledstartdate,
+       (
+           CASE
+               WHEN j.jobtype LIKE 'Application'
+               THEN 'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=1239699&objectHandle=' || j.
+               jobid || '&processHandle=' || proc.processid || '&paneId=1239699_151'
+               WHEN j.jobtype LIKE 'Renewal'
+               THEN 'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=1243107&objectHandle=' || j.
+               jobid || '&processHandle=' || proc.processid || '&paneId=1243107_175'
+           END
+       ) processlink
+FROM g_mvw_bl_jobs j,
+     g_mvw_processes proc
+WHERE j.jobid = proc.jobid
+      AND j.jobstatus NOT IN (
+    'Approved',
+    'Denied',
+    'Draft',
+    'Withdrawn',
+    'More Information Required'
+)
+      AND proc.datecompleted IS NULL
+      AND proc.processtype <> 'Pay Fees'
