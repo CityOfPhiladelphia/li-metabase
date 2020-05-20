6e332b976f9f0212dde0b47c6f478cc9e001cf87
Philip Ribbens <Philip.Ribbens@phila.gov>
2020-05-20 08:55:51 -0400

    Format query

--- etl/UpdateTables/queries/licenses/licenses_issued_tl.sql
+++ etl/UpdateTables/queries/licenses/licenses_issued_tl.sql
@@ -1,26 +1,26 @@
-SELECT DISTINCT lic.licensetype LicenseType,
-  lic.LicenseNumber,
-  ap.externalfilenum JobNumber,
-  ap.applicationtype JobType,
-  lic.LICENSEISSUEDATE IssueDate
+SELECT DISTINCT lic.licensetype licensetype,
+                lic.licensenumber,
+                ap.externalfilenum jobnumber,
+                ap.applicationtype jobtype,
+                lic.licenseissuedate issuedate
 FROM lmscorral.tl_tradelicenses lic,
-  query.j_tl_application ap
-WHERE lic.objectid    = ap.tradelicenseobjectid (+)
-AND lic.LICENSEISSUEDATE >= '01-JAN-17'
-AND lic.LICENSEISSUEDATE  < SYSDATE
-AND ap.statusdescription LIKE 'Approved'
+     query.j_tl_application ap
+WHERE lic.objectid = ap.tradelicenseobjectid (+)
+      AND lic.licenseissuedate >= '01-JAN-17'
+      AND lic.licenseissuedate < sysdate
+      AND ap.statusdescription LIKE 'Approved'
 UNION
-SELECT DISTINCT lic.licensetype LicenseType,
-  lic.licensenumber,
-  tar.externalfilenum JobNumber,
-  tar.applicationtype JobType,
-  tar.completeddate issuedate
+SELECT DISTINCT lic.licensetype licensetype,
+                lic.licensenumber,
+                tar.externalfilenum jobnumber,
+                tar.applicationtype jobtype,
+                tar.completeddate issuedate
 FROM lmscorral.tl_tradelicenses lic,
-  query.r_tl_amendrenew_license ar,
-  query.j_tl_amendrenew tar
-WHERE lic.objectid     = ar.licenseid (+)
-AND ar.amendrenewid    = tar.objectid (+)
-AND tar.completeddate >= '01-JAN-17'
-AND tar.completeddate  < SYSDATE
-AND tar.statusdescription LIKE 'Approved'
-AND tar.applicationtype LIKE 'Renewal'
+     query.r_tl_amendrenew_license ar,
+     query.j_tl_amendrenew tar
+WHERE lic.objectid = ar.licenseid (+)
+      AND ar.amendrenewid = tar.objectid (+)
+      AND tar.completeddate >= '01-JAN-17'
+      AND tar.completeddate < sysdate
+      AND tar.statusdescription LIKE 'Approved'
+      AND tar.applicationtype LIKE 'Renewal'
