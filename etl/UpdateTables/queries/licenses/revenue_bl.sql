364cfdff64bcd9f60505d19ef4d29284e6df6af6
Philip Ribbens <Philip.Ribbens@phila.gov>
2020-05-22 14:35:05 -0400

    Fix revenue_bl amounts

--- etl/UpdateTables/queries/licenses/revenue_bl.sql
+++ etl/UpdateTables/queries/licenses/revenue_bl.sql
@@ -1,11 +1,16 @@
-SELECT DISTINCT j.jobnumber,
-                j.jobtype,
-                pay.paymentreceiveddate paymentdate,
-                fee.feebaseamount amount,
-                fee.feetype
+SELECT j.jobnumber,
+       j.jobtype,
+       pay.maxpaymentreceiveddate paymentdate,
+       pay.totalpaymentamount amount,
+       fee.feetype
 FROM g_mvw_bl_jobs j,
      g_mvw_fee fee,
-     g_mvw_payment pay
+     (SELECT feeobjectid,
+             SUM (paymentdistamount) totalpaymentamount,
+             MAX (paymentreceiveddate) maxpaymentreceiveddate
+      FROM g_mvw_payment pay
+      GROUP BY feeobjectid
+     ) pay
 WHERE j.jobid = fee.referencedobjectid
       AND fee.feeobjectid = pay.feeobjectid
-      AND pay.paymentreceiveddate >= add_months (trunc (sysdate, 'MM'), - 25)
+      AND pay.maxpaymentreceiveddate >= add_months (trunc (sysdate, 'MM'), - 25)
