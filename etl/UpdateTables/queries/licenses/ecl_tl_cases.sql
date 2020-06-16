d9ebecc19648729ae838d186860c859e674099d6
Philip Ribbens <Philip.Ribbens@phila.gov>
2020-06-16 11:36:59 -0400

    Give licenselink field values

--- etl/UpdateTables/queries/licenses/ecl_tl_cases.sql
+++ etl/UpdateTables/queries/licenses/ecl_tl_cases.sql
@@ -5,7 +5,7 @@ SELECT tl.licensenumber,
        tl.issuedate licenseissuedate,
        tl.expirationdate licenseexpirationdate,
        upper (tl.status) licensestatus,
-       NULL AS licenselink, --'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=2855291&objectHandle=' || tl.objectid AS licenselink ,
+       'https://eclipseprod.phila.gov/phillylmsprod/int/lms/Default.aspx#presentationId=2855291&objectHandle=' || tl.objectid AS licenselink ,
        cases.casenumber,
        cases.listedas,
        cases.casestatus,
@@ -20,6 +20,5 @@ SELECT tl.licensenumber,
 FROM g_mvw_trade_licenses tl,
      mvw_tl_cases cases
 WHERE tl.licensenumber = cases.licensenumber (+)
-      --AND tl.licensenumber = '39939'
 ORDER BY licensenumber,
          casenumber
