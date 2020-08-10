8899d4d11c301ca04c67678bf28fcc94ba893dc3
Philip Ribbens <Philip.Ribbens@phila.gov>
2020-08-10 14:53:40 -0400

    Add open cases queries

---
+++ etl/UpdateTables/queries/compliance_enforcement/open_cases.sql
@@ -0,0 +1,104 @@
+SELECT c.internalid,
+       c.casenumber,
+       (
+           CASE
+               WHEN c.casetype IS NULL
+               THEN '(none)'
+               ELSE c.casetype
+           END
+       ) casetype,
+       (
+           CASE
+               WHEN c.caseresponsibility IS NULL
+               THEN '(none)'
+               ELSE c.caseresponsibility
+           END
+       ) caseresponsibility,
+       (
+           CASE
+               WHEN c.casepriority IS NULL
+               THEN '(none)'
+               ELSE c.casepriority
+           END
+       ) casepriority,
+       c.createddate,
+       c.completeddate,
+       (
+           CASE
+               WHEN c.completeddate IS NULL
+               THEN 'Incomplete'
+               ELSE 'Completed'
+           END
+       ) completed,
+       c.casestatus,
+       (
+           CASE
+               WHEN c.firstcompletedinv IS NULL
+               THEN 'Uninvestigated'
+               ELSE 'Investigated'
+           END
+       ) investigated,
+       c.firstcompletedinv,
+       (
+           CASE
+               WHEN c.firstcompletedinvstatus IS NULL
+               THEN '(n/a - no completed investigations)'
+               ELSE c.firstcompletedinvstatus
+           END
+       ) firstcompletedinvstatus,
+       (
+           CASE
+               WHEN c.firstcompletedinvinvestigator IS NULL
+               THEN '(n/a - no completed investigations)'
+               ELSE c.firstcompletedinvinvestigator
+           END
+       ) firstcompletedinvinvestigator,
+       c.lastcompletedinv,
+       c.lastcompletedinvstatus,
+       (
+           CASE
+               WHEN c.lastcompletedinvinvestigator IS NULL
+               THEN '(n/a - no completed investigations)'
+               ELSE c.lastcompletedinvinvestigator
+           END
+       ) lastcompletedinvinvestigator,
+       c.nextscheduledinv,
+       (
+           CASE
+               WHEN c.nextscheduledinvinvestigator IS NULL
+               THEN '(n/a - no upcoming investigations)'
+               ELSE c.nextscheduledinvinvestigator
+           END
+       ) nextscheduledinvinvestigator,
+       c.overdueinvscheduleddate,
+       (
+           CASE
+               WHEN c.overdueinvinvestigator IS NULL
+               THEN '(n/a - no overdue investigations)'
+               ELSE c.overdueinvinvestigator
+           END
+       ) overdueinvinvestigator,
+       c.onlyincludeslicensevios,
+       c.address,
+       (
+           CASE
+               WHEN c.zip IS NULL
+                    OR c.zip = ' '
+               THEN '(none)'
+               ELSE c.zip
+           END
+       ) zip,
+       c.council_district,
+       c.li_district,
+       c.censustract,
+       sdo_cs.transform (sdo_geometry (2001, 2272, sdo_point_type (c.geocode_x, c.geocode_y, NULL), NULL, NULL), 4326).sdo_point.
+       x lon,
+       sdo_cs.transform (sdo_geometry (2001, 2272, sdo_point_type (c.geocode_x, c.geocode_y, NULL), NULL, NULL), 4326).sdo_point.
+       y lat,
+       c.systemofrecord
+FROM mvw_cases c
+WHERE c.casestatus IN (
+    'Under Investigation',
+    'In Violation',
+    'Open'
+)
---
+++ etl/UpdateTables/queries/compliance_enforcement/open_cases_bus_days.sql
@@ -0,0 +1,141 @@
+SELECT c.internalid,
+       c.casenumber,
+       c.casetype,
+       c.caseresponsibility,
+       c.casepriority,
+       c.createddate,
+       c.casestatus,
+       (
+           CASE
+               WHEN c.completeddate IS NULL
+               THEN bds1.businessdayssince - bds2.businessdayssince
+               ELSE bds6.businessdayssince - bds2.businessdayssince
+           END
+       ) bdopen,
+       (
+           CASE
+               WHEN c.completeddate IS NULL
+               THEN (
+                   CASE
+                       WHEN bds1.businessdayssince - bds2.businessdayssince <= 10
+                       THEN '0-10'
+                       WHEN bds1.businessdayssince - bds2.businessdayssince BETWEEN 11 AND 20
+                       THEN '11-20'
+                       WHEN bds1.businessdayssince - bds2.businessdayssince BETWEEN 21 AND 100
+                       THEN '21-100'
+                       ELSE 'More than 100'
+                   END
+               )
+               ELSE (
+                   CASE
+                       WHEN bds6.businessdayssince - bds2.businessdayssince <= 10
+                       THEN '0-10'
+                       WHEN bds6.businessdayssince - bds2.businessdayssince BETWEEN 11 AND 20
+                       THEN '11-20'
+                       WHEN bds6.businessdayssince - bds2.businessdayssince BETWEEN 21 AND 100
+                       THEN '21-100'
+                       ELSE 'More than 100'
+                   END
+               )
+           END
+       ) bdopencategories,
+       c.completeddate,
+       c.completed,
+       c.investigated,
+       c.firstcompletedinv,
+       c.firstcompletedinvstatus,
+       c.firstcompletedinvinvestigator,
+       c.lastcompletedinv,
+       (
+           CASE
+               WHEN c.lastcompletedinv IS NULL
+               THEN NULL
+               ELSE bds1.businessdayssince - bds3.businessdayssince
+           END
+       ) bdsincelastcomplinv,
+       (
+           CASE
+               WHEN c.lastcompletedinv IS NULL
+               THEN NULL
+               WHEN bds1.businessdayssince - bds3.businessdayssince <= 10
+               THEN '0-10'
+               WHEN bds1.businessdayssince - bds3.businessdayssince BETWEEN 11 AND 20
+               THEN '11-20'
+               WHEN bds1.businessdayssince - bds3.businessdayssince BETWEEN 21 AND 100
+               THEN '21-100'
+               ELSE 'More than 100'
+           END
+       ) bdsincelastcomplinvcategories,
+       c.lastcompletedinvstatus,
+       c.lastcompletedinvinvestigator,
+       c.nextscheduledinv,
+       (
+           CASE
+               WHEN c.nextscheduledinv IS NULL
+               THEN NULL
+               ELSE bds4.businessdayssince - bds1.businessdayssince
+           END
+       ) bduntilnextscheduledinv,
+       (
+           CASE
+               WHEN c.nextscheduledinv IS NULL
+               THEN NULL
+               WHEN bds4.businessdayssince - bds1.businessdayssince <= 10
+               THEN '0-10'
+               WHEN bds4.businessdayssince - bds1.businessdayssince BETWEEN 11 AND 20
+               THEN '11-20'
+               WHEN bds4.businessdayssince - bds1.businessdayssince BETWEEN 21 AND 100
+               THEN '21-100'
+               ELSE 'More than 100'
+           END
+       ) bduntilnextschedinvcategories,
+       c.nextscheduledinvinvestigator,
+       c.overdueinvscheduleddate,
+       (
+           CASE
+               WHEN c.overdueinvscheduleddate IS NULL
+               THEN NULL
+               ELSE bds1.businessdayssince - bds5.businessdayssince
+           END
+       ) bdsinceoverdueinv,
+       (
+           CASE
+               WHEN c.overdueinvscheduleddate IS NULL
+               THEN NULL
+               WHEN bds1.businessdayssince - bds5.businessdayssince <= 10
+               THEN '0-10'
+               WHEN bds1.businessdayssince - bds5.businessdayssince BETWEEN 11 AND 20
+               THEN '11-20'
+               WHEN bds1.businessdayssince - bds5.businessdayssince BETWEEN 21 AND 100
+               THEN '21-100'
+               ELSE 'More than 100'
+           END
+       ) bdsinceoverdueinvcategories,
+       c.overdueinvinvestigator,
+       c.onlyincludeslicensevios,
+       c.address,
+       c.zip,
+       c.council_district,
+       c.li_district,
+       c.censustract,
+       c.lon,
+       c.lat,
+       c.systemofrecord
+FROM open_cases c,
+     business_days_since_2007 bds1,
+     business_days_since_2007 bds2,
+     business_days_since_2007 bds3,
+     business_days_since_2007 bds4,
+     business_days_since_2007 bds5,
+     business_days_since_2007 bds6
+WHERE to_date (to_char (sysdate, 'mm') || to_char (sysdate, 'dd') || to_char (sysdate, 'yyyy'), 'MMDDYYYY') = bds1.dateofyear (+)
+      AND to_date (to_char (c.createddate, 'mm') || to_char (c.createddate, 'dd') || to_char (c.createddate, 'yyyy'), 'MMDDYYYY')                          =
+      bds2.dateofyear (+)
+      AND to_date (to_char (c.lastcompletedinv, 'mm') || to_char (c.lastcompletedinv, 'dd') || to_char (c.lastcompletedinv, 'yyyy'
+      ), 'MMDDYYYY')   = bds3.dateofyear (+)
+      AND to_date (to_char (c.nextscheduledinv, 'mm') || to_char (c.nextscheduledinv, 'dd') || to_char (c.nextscheduledinv, 'yyyy'
+      ), 'MMDDYYYY')   = bds4.dateofyear (+)
+      AND to_date (to_char (c.overdueinvscheduleddate, 'mm') || to_char (c.overdueinvscheduleddate, 'dd') || to_char (c.overdueinvscheduleddate
+      , 'yyyy'), 'MMDDYYYY') = bds5.dateofyear (+)
+      AND to_date (to_char (c.completeddate, 'mm') || to_char (c.completeddate, 'dd') || to_char (c.completeddate, 'yyyy'), 'MMDDYYYY'
+      )            = bds6.dateofyear (+)
