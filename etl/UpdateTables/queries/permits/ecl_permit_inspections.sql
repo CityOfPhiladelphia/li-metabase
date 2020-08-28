23d007a3c87dd65572b897237ecf80b4aa3ea2c6
Jessica Bradley <Jessica.Bradley@phila.gov>
2020-08-28 15:25:33 -0400

    big changes for more categorization in metabase

--- etl/UpdateTables/queries/permits/ecl_permit_inspections.sql
+++ etl/UpdateTables/queries/permits/ecl_permit_inspections.sql
@@ -6,30 +6,22 @@ SELECT
     i.inspectionrequesteddate            inspectionrequesteddate,
     i.inspectionscheduledstartdate       inspectionscheduleddate,
     i.inspectioncompleteddate            inspectioncompleteddate,
-    (
-        CASE
-            WHEN i.inspectionoutcome IS NULL THEN
-                '(none)'
-            ELSE
-                i.inspectionoutcome
-        END
-    ) inspectionoutcome,
+    i.inspectionoutcome                  inspectionoutcome,
     i.inspectorcomments                  inspectorcomments,
-    (
-        CASE
-            WHEN i.inspectorname IS NULL THEN
-                '(none)'
-            ELSE
-                i.inspectorname
-        END
-    ) inspectorname,
+    CASE
+        WHEN i.inspectorname IS NULL THEN
+            '(none)'
+        ELSE
+            i.inspectorname
+    END inspectorname,
+    first_insp.inspectioncompleteddate   initialinspectiondate,
+    p.mostrecentinsp                     mostrecentinsp,
     i.permitnumber                       permitnumber,
     i.permittype                         permittype,
     i.permitstatus                       permitstatus,
-    p.issuedate                          issuedate,
-    p.mostrecentinsp                     mostrecentinsp,
-    p.applicanttype                      applicanttype,
-    p.typeofwork                         typeofwork,
+    p.issuedate                          permitissuedate,
+    p.applicanttype                      permitapplicanttype,
+    p.typeofwork                         permittypeofwork,
     CASE
         WHEN trunc(i.inspectioncompleteddate) BETWEEN '31-MAR-2020' AND '01-JUL-2020' THEN
             'FY20Q4'
@@ -55,6 +47,8 @@ SELECT
             'FY22Q3'
         WHEN trunc(i.inspectioncompleteddate) BETWEEN '31-MAR-2022' AND '01-JUL-2022' THEN
             'FY22Q4'
+        WHEN trunc(i.inspectioncompleteddate) IS NULL THEN
+            '(none)'
     END inspcompleteddatefiscal,
     CASE
         WHEN trunc(i.inspectionscheduledstartdate) BETWEEN '31-MAR-2020' AND '01-JUL-2020' THEN
@@ -81,25 +75,107 @@ SELECT
             'FY22Q3'
         WHEN trunc(i.inspectionscheduledstartdate) BETWEEN '31-MAR-2022' AND '01-JUL-2022' THEN
             'FY22Q4'
+        WHEN trunc(i.inspectionscheduledstartdate) IS NULL THEN
+            '(none)'
     END inspscheduleddatefiscal,
-    trunc(sysdate) - trunc(p.mostrecentinsp) AS dayssincemostrecinsp,
+    case 
+        when p.mostrecentinsp is null then 0
+        else trunc(sysdate) - trunc(p.mostrecentinsp) 
+    end dayssincemostrecinsp,
+    case 
+        when p.mostrecentinsp is null then 'n/a - not inspected'
+        when trunc(sysdate) - trunc(p.mostrecentinsp) < 0 then 'n/a - not inspected'
+        when trunc(sysdate) - trunc(p.mostrecentinsp) <= 5 then '0-5'
+        when trunc(sysdate) - trunc(p.mostrecentinsp) <= 20 then '6-20'
+        when trunc(sysdate) - trunc(p.mostrecentinsp) <= 90 then '21-90'
+        else '90+'
+    end dayssincemostrecinspcat,
     CASE
         WHEN trunc(i.inspectioncompleteddate) IS NULL
              AND ( trunc(i.inspectionscheduledstartdate) < trunc(sysdate) ) THEN
             trunc(sysdate) - trunc(i.inspectionscheduledstartdate)
-            || ' DAYS OVERDUE'
         ELSE
-            'NOT OVERDUE'
+            0
     END overdueinspection,
     CASE
-        WHEN trunc(i.inspectioncompleteddate) > trunc(i.inspectionscheduledstartdate) -- and Cast(i.inspectioncompleteddate as DATE) <> cast(i.inspectionscheduledstartdate as date)
-         THEN
+        WHEN i.inspectionscheduledstartdate IS NULL THEN
+            'n/a - no scheduled start date'
+        when trunc(i.inspectioncompleteddate) is not null and trunc(i.inspectionscheduledstartdate) - trunc(i.inspectioncompleteddate) < 0 then 'n/a - completed on time'
+        when i.inspectioncompleteddate is not null then 'n/a - completed on time'
+        WHEN (trunc(i.inspectioncompleteddate) IS NULL AND (trunc(sysdate) - trunc(i.inspectionscheduledstartdate))  < 0 )
+        THEN 'n/a - scheduled start date upcoming'
+        WHEN trunc(i.inspectioncompleteddate) IS NULL
+             AND ( trunc(sysdate) - trunc(i.inspectionscheduledstartdate) ) <= 5 THEN
+            '0-5'
+        WHEN trunc(i.inspectioncompleteddate) IS NULL
+             AND ( trunc(sysdate) - trunc(i.inspectionscheduledstartdate) ) <= 20 THEN
+            '6-20'
+        WHEN trunc(i.inspectioncompleteddate) IS NULL
+             AND ( trunc(sysdate) - trunc(i.inspectionscheduledstartdate) ) <= 90 THEN
+            '21-90'
+        ELSE
+            '90+'
+    END overdueinspectioncat,
+    CASE
+        WHEN i.inspectioncompleteddate IS NULL THEN
+            'n/a - no completed date'
+        WHEN trunc(i.inspectioncompleteddate - i.inspectionscheduledstartdate) < 0 THEN
+            'n/a'
+        WHEN trunc(i.inspectioncompleteddate - i.inspectionscheduledstartdate) <= 5 THEN
+            '0-5'
+        WHEN trunc(i.inspectioncompleteddate - i.inspectionscheduledstartdate) <= 20 THEN
+            '6-20'
+        WHEN trunc(i.inspectioncompleteddate - i.inspectionscheduledstartdate) <= 90 THEN
+            '21-90'
+        ELSE
+            '90+'
+    END daysinspcompafterscheduledcat,
+    CASE
+        WHEN trunc(i.inspectioncompleteddate) > trunc(i.inspectionscheduledstartdate) THEN
             abs(trunc(i.inspectioncompleteddate) - trunc(i.inspectionscheduledstartdate))
         ELSE
             0
     END daysinspcompafterscheduled,
-    first_insp.inspectioncompleteddate   initialinspectiondate,
-    trunc(sysdate - first_insp.inspectioncompleteddate) dayssinceinitialinspection
+    CASE
+        WHEN first_insp.inspectioncompleteddate IS NULL THEN
+            'n/a - no initial inspection'
+        WHEN trunc(sysdate - first_insp.inspectioncompleteddate) < 0 THEN
+            'n/a'
+        WHEN trunc(sysdate - first_insp.inspectioncompleteddate) <= 5 THEN
+            '0-5'
+        WHEN trunc(sysdate - first_insp.inspectioncompleteddate) <= 20 THEN
+            '6-20'
+        WHEN trunc(sysdate - first_insp.inspectioncompleteddate) <= 90 THEN
+            '21-90'
+        ELSE
+            '90+'
+    END dayssinceinitialinspectioncat,
+    case when i.inspectioncompleteddate IS NULL then 0
+        else trunc(i.inspectioncompleteddate - p.issuedate)
+    end issuancetocompletion,
+    CASE
+        WHEN i.inspectioncompleteddate IS NULL THEN
+            'n/a - no completed date'
+        WHEN trunc(i.inspectioncompleteddate - p.issuedate) < 0 THEN
+            'n/a'
+        WHEN trunc(i.inspectioncompleteddate - p.issuedate) <= 5 THEN
+            '0-5'
+        WHEN trunc(i.inspectioncompleteddate - p.issuedate) <= 20 THEN
+            '6-20'
+        WHEN trunc(i.inspectioncompleteddate - p.issuedate) <= 90 THEN
+            '21-90'
+        ELSE
+            '90+'
+    END issuancetocompletioncat,
+    trunc(sysdate - first_insp.inspectioncompleteddate) dayssinceinitinspection,
+    case 
+        when first_insp.inspectioncompleteddate is null then 'n/a - no initial inspection'
+        when trunc(sysdate - first_insp.inspectioncompleteddate) < 0 then 'n/a'
+        when trunc(sysdate - first_insp.inspectioncompleteddate) <= 5 then '0-5'
+        when trunc(sysdate - first_insp.inspectioncompleteddate) <= 20 then '6-20'
+        when trunc(sysdate - first_insp.inspectioncompleteddate) <= 90 then '21-90'
+        else '90+'
+    End dayssinceinitinspectioncat
 FROM
     g_mvw_permits     p,
     g_mvw_perm_insp   i,
@@ -131,6 +207,6 @@ FROM
 WHERE
     i.jobid = p.jobid
     AND p.permitnumber = first_insp.permitnumber (+)
-    AND ( p.permittype <> 'Zoning'
-          OR ( p.permittype = 'Zoning'
-               AND p.typeofwork = 'Change of Use' ) )
+        AND ( p.permittype <> 'Zoning'
+              OR ( p.permittype = 'Zoning'
+                   AND p.typeofwork = 'Change of Use' ) )
